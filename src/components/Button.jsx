5d09a052afa9d1d9dc5a60872358d088f1246ab7
Aayush Sharma <your-email@example.com>
2026-02-14 17:19:10 +0530

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+
+const Button = ( {text, className, }) => {
+  return (
+   <a 
+   onClick={(e) =>{
+    e.preventDefault();
+
+    const target = document.getElementById('counter')
+
+    if(target && id ) {
+        const offset =window.innerHeight * 0.15;
+
+        const
+    }
+   }
+   }
+   className={`${className ?? ''} cta-wrapper`} >
+    <div className="cta-button group">
+        <div className="bg-circle"/>
+        <p className="text">{text}</p>
+        <div className="arrow-wrapper">
+            <img src="/public/images/arrow-down.svg" alt="arrow" />
+        </div>
+    </div>
+
+   </a>
+  )
+}
+
+export default Button
